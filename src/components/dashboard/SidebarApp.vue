c9138d990f76f714a704d9840eff5d3dd7308173
alicedev94 <alicedev94@gmail.com>
2025-01-13 19:32:33 -0400

    Adjust template

--- src/components/dashboard/SidebarApp.vue
+++ src/components/dashboard/SidebarApp.vue
@@ -1,176 +1,113 @@
 <script setup lang="ts">
-  import { onMounted, ref } from 'vue';
-  import { initFlowbite } from 'flowbite';
-  import { useAuthStore } from '@/stores/Auth';
-  import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import { initFlowbite } from 'flowbite';
+import { useAuthStore } from '@/stores/Auth';
+import axios from 'axios';
 
-  // pinia
-  const authStore = useAuthStore();
-  const ID_Rol = authStore.user?.ID_Rol;
-  const dataBaseUrlRol = `${import.meta.env.VITE_URL}/rol`;
-  const roles = ref();
-  const permisos = ref();
+// pinia
+const authStore = useAuthStore();
+const ID_Rol = authStore.user?.ID_Rol;
+const dataBaseUrlRol = `${import.meta.env.VITE_URL}/rol`;
+const roles = ref();
+const permisos = ref();
+const items = ref();
 
-  const getRoles = async () => {
-    try {
-      const response = await axios.get(
-        `${dataBaseUrlRol}/filterRoles/${ID_Rol}`
-      );
-      roles.value = response.data;
-      permisos.value = roles.value.Id_Permissions;
-    } catch (error) {
-      console.log(error);
+const getRoles = async () => {
+  try {
+    const response = await axios.get(
+      `${dataBaseUrlRol}/filterRoles/${ID_Rol}`
+    );
+    roles.value = response.data;
+    permisos.value = roles.value.Id_Permissions;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const hasPermission = (id: number): boolean => {
+  {
+    if (!roles.value || !roles.value.Id_Permissions) {
+      return false;
     }
-  };
+    const permissions = permisos.value
+      .split(',')
+      .map((permission: string) => parseInt(permission.trim()));
+    return permissions.includes(id);
+  }
+};
 
-  const hasPermission = (id: number): boolean => {
-    {
-      if (!roles.value || !roles.value.Id_Permissions) {
-        return false;
-      }
-      const permissions = permisos.value
-        .split(',')
-        .map((permission: string) => parseInt(permission.trim()));
-      return permissions.includes(id);
+// Alicedev94 intervention
+const getItems = async () => {
+  try {
+    // Change to production URL
+    const { data } = await axios.get(
+      `http://localhost:3000/api/v1/navigation`
+    );
+    if (!data) {
+      throw new Error(`Fail to get items: ${data.error}`);
     }
-  };
 
-  // inicializar librería flowBite
-  onMounted(async () => {
-    await getRoles();
-    initFlowbite();
-  });
+    return { success: true, data };
+  } catch (error) {
+    return { success: false, error };
+  }
+};
 
-  // cerrar session
-  const handleLogout = () => {
-    authStore.logout();
-  };
+// inicializar librería flowBite
+onMounted(async () => {
+  await getRoles();
+  let responseItems = await getItems();
+  items.value = responseItems?.data?.data ?? '';
+  console.log(items.value);
+  initFlowbite();
+});
+
+// cerrar session
+const handleLogout = () => {
+  authStore.logout();
+};
 </script>
 
 <template>
-  <aside
-    id="logo-sidebar"
+  <aside id="logo-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
-    aria-label="Sidebar"
-  >
-    <div
-      class="flex flex-col justify-between h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800"
-    >
-      <ul class="space-y-2 font-medium">
-        <li>
-          <RouterLink
-            to="/dashboard"
-            class="flex items-center p-2 text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span class="ms-3">Dashboard</span>
-          </RouterLink>
-        </li>
-        <li v-if="hasPermission(1)">
-          <RouterLink
-            to="/tables"
-            class="flex items-center p-2 text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span class="flex-1 ms-3 whitespace-nowrap">Tablas</span>
-          </RouterLink>
-        </li>
-        <li v-if="hasPermission(2)">
-          <RouterLink
-            to="/tablesExhibition"
-            class="flex items-center p-2 text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span class="flex-1 ms-3 whitespace-nowrap">Exhibición</span>
-            <span
-              class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"
-              >3</span
-            >
-          </RouterLink>
-        </li>
-        <li v-if="hasPermission(1)">
-          <button
-            type="button"
-            class="flex items-center w-full p-2 text-base text-gray-400 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-            aria-controls="dropdown-example"
-            data-collapse-toggle="dropdown-example"
-          >
-            <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap"
-              >Usuarios</span
-            >
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
-          <ul id="dropdown-example" class="hidden py-2 space-y-2">
-            <li>
-              <RouterLink
-                to="/tables"
-                class="flex items-center w-full p-2 text-gray-400 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              >
-                Maestro de usuarios
-              </RouterLink>
-            </li>
-            <li>
-              <RouterLink
-                to="/tablesRoles"
-                class="flex items-center w-full p-2 text-gray-400 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-              >
-                Maestro de Roles
-              </RouterLink>
-            </li>
-          </ul>
-        </li>
+    aria-label="Sidebar">
+    <div class="flex flex-col justify-between h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
 
-        <li v-if="hasPermission(3)">
-          <RouterLink
-            to="#"
-            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <svg
-              class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 18 20"
-            >
-              <path
-                d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"
-              />
-            </svg>
-            <span class="flex-1 ms-3 whitespace-nowrap">Lead Time</span>
-          </RouterLink>
-        </li>
-      </ul>
+      <!-- Items -->
+      <div>
+        <div v-for="item in items">
+          <div v-if="item.children && item.children.length">
+            <div
+              class="flex items-center p-2 text-dark font-semibold  rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <span class="ms-3">{{ item.name }}</span>
+          </div>
+
+            <div v-for="child in item.children">
+              <RouterLink :to="child.link"
+                class="flex items-center p-2 text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span class="ms-3">{{ child.name }}</span>
+              </RouterLink>
+
+            </div>
+          </div>
+          <div v-else>
+            <RouterLink :to="item.link"
+              class="flex items-center p-2 text-dark font-semibold rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <span class="ms-3">{{ item.name }}</span>
+            </RouterLink>
+          </div>
+        </div>
+      </div>
 
       <!-- this is singup, please add to commponent -->
-      <div
-        @click="handleLogout"
-        class="flex items-center p-2 cursor-pointer text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-      >
+      <div @click="handleLogout"
+        class="flex items-center p-2 cursor-pointer text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
         <svg
           class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 18 16"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
-          />
+          aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
         </svg>
         <span class="flex-1 ms-3 whitespace-nowrap">Cerrar sesión</span>
       </div>
--- src/views/formAuth/LoginApp.vue
+++ src/views/formAuth/LoginApp.vue
@@ -1,92 +1,99 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useAuthStore } from '../../stores/Auth';
-import { onMounted } from 'vue';
-import { initFlowbite } from 'flowbite';
-import { useLocalStorage } from '@vueuse/core';
-import { useAppStore } from '@/stores/App';
+  import { ref } from 'vue';
+  import { useAuthStore } from '../../stores/Auth';
+  import { onMounted } from 'vue';
+  import { initFlowbite } from 'flowbite';
+  import { useLocalStorage } from '@vueuse/core';
+  import { useAppStore } from '@/stores/App';
 
-const username = ref<string>('');
-const password = ref<string>('');
-const rememberMe = ref<boolean>(false);
-const storedEmail = useLocalStorage('Email', '');
-const storedPassword = useLocalStorage('Password', '');
-const emailError = ref<string | null>(null);
-const passwordError = ref<string | null>(null);
+  const username = ref<string>('');
+  const password = ref<string>('');
+  const rememberMe = ref<boolean>(false);
+  const storedEmail = useLocalStorage('Email', '');
+  const storedPassword = useLocalStorage('Password', '');
+  const emailError = ref<string | null>(null);
+  const passwordError = ref<string | null>(null);
 
-const authStore = useAuthStore();
-const appStore = useAppStore();
+  const authStore = useAuthStore();
+  const appStore = useAppStore();
 
-// initialize components based on data attribute selectors
-onMounted(() => {
-  initFlowbite();
+  // initialize components based on data attribute selectors
+  onMounted(() => {
+    initFlowbite();
 
-  if (storedEmail.value && storedPassword.value) {
-    username.value = storedEmail.value;
-    password.value = storedPassword.value;
-    rememberMe.value = true;
-  }
-});
-
-const useLocalStorageAuthentication = () => {
-  if (rememberMe.value) {
-    storedEmail.value = username.value;
-    storedPassword.value = password.value;
-  } else {
-    storedEmail.value = '';
-    storedPassword.value = '';
-  }
-};
-
-const validateEmail = (email: string) => {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return re.test(email);
-};
-
-const handleLogin = async () => {
-  emailError.value = null;
-  passwordError.value = null;
-
-  if (!validateEmail(username.value)) {
-    emailError.value = 'Correo electrónico no válido';
-  }
-
-  if (!password.value) {
-    passwordError.value = 'La contraseña no puede estar vacía';
-  }
-
-  if (!emailError.value && !passwordError.value) {
-    useLocalStorageAuthentication();
-    authStore.clearError(); // Limpiar errores anteriores
-    try {
-      await authStore.login({
-        Email: username.value,
-        Password: password.value,
-      });
-    } catch (error) {
-      console.log('error');
+    if (storedEmail.value && storedPassword.value) {
+      username.value = storedEmail.value;
+      password.value = storedPassword.value;
+      rememberMe.value = true;
     }
-  }
-};
+  });
+
+  const useLocalStorageAuthentication = () => {
+    if (rememberMe.value) {
+      storedEmail.value = username.value;
+      storedPassword.value = password.value;
+    } else {
+      storedEmail.value = '';
+      storedPassword.value = '';
+    }
+  };
+
+  const validateEmail = (email: string) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
+
+  const handleLogin = async () => {
+    emailError.value = null;
+    passwordError.value = null;
+
+    if (!validateEmail(username.value)) {
+      emailError.value = 'Correo electrónico no válido';
+    }
+
+    if (!password.value) {
+      passwordError.value = 'La contraseña no puede estar vacía';
+    }
+
+    if (!emailError.value && !passwordError.value) {
+      useLocalStorageAuthentication();
+      authStore.clearError(); // Limpiar errores anteriores
+      try {
+        await authStore.login({
+          Email: username.value,
+          Password: password.value,
+        });
+      } catch (error) {
+        console.log('error');
+      }
+    }
+  };
 </script>
 
 <template>
   <div class="relative h-full w-full bg-gray-50">
     <div
-      class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#0000001a_1px,transparent_1px),linear-gradient(to_bottom,#0000001a_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#fff_70%,transparent_100%)]">
-    </div>
+      class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#0000001a_1px,transparent_1px),linear-gradient(to_bottom,#0000001a_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#fff_70%,transparent_100%)]"
+    ></div>
 
     <div class="grid h-screen place-items-center">
       <div class="max-w-lg w-full z-10">
-        <div style="
+        <div
+          style="
             box-shadow:
               0 10px 25px -5px rgba(0, 0, 0, 0.1),
               0 10px 10px -5px rgba(0, 0, 0, 0.04);
-          " class="bg-white border-gray-200 rounded-lg shadow-xl overflow-hidden">
+          "
+          class="bg-white border-gray-200 rounded-lg shadow-xl overflow-hidden"
+        >
           <div class="p-8 flex flex-col justify-center">
             <!-- logo -->
             <div class="flex justify-center">
-              <img src="@/assets/img/daka.png" class="w-32 rounded-lg" alt="Logo de tiendas daka" />
+              <img
+                src="@/assets/img/daka.png"
+                class="w-32 rounded-lg"
+                alt="Logo de tiendas daka"
+              />
             </div>
 
             <div>
@@ -100,29 +107,49 @@ const handleLogin = async () => {
             <form @submit.prevent="handleLogin" class="space-y-6">
               <div class="rounded-md shadow-sm">
                 <div>
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-700">
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-700"
+                  >
                     Correo Electrónico
                   </label>
-                  <input v-model="username" placeholder="ejemplo@tiendasdaka.com" :class="{
-                    'border-red-500  text-red-900 placeholder-red-700':
-                      emailError,
-                  }"
+                  <input
+                    v-model="username"
+                    placeholder="ejemplo@tiendasdaka.com"
+                    :class="{
+                      'border-red-500  text-red-900 placeholder-red-700':
+                        emailError,
+                    }"
                     class="appearance-none relative block w-full px-3 py-3 border border-gray-300 bg-white text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    autocomplete="email" type="text" name="email" id="email" />
+                    autocomplete="email"
+                    type="text"
+                    name="email"
+                    id="email"
+                  />
                   <p v-if="emailError" class="mt-2 text-sm text-red-600">
                     <span class="font-medium"></span> {{ emailError }}
                   </p>
                 </div>
                 <div class="mt-4">
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-700">
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-700"
+                  >
                     Contraseña
                   </label>
-                  <input v-model="password" placeholder="123456" :class="{
-                    'border-red-500  text-red-900 placeholder-red-700':
-                      passwordError,
-                  }"
+                  <input
+                    v-model="password"
+                    placeholder="123456"
+                    :class="{
+                      'border-red-500  text-red-900 placeholder-red-700':
+                        passwordError,
+                    }"
                     class="appearance-none relative block w-full px-3 py-3 border border-gray-300 bg-white text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    autocomplete="current-password" type="password" name="password" id="password" />
+                    autocomplete="current-password"
+                    type="password"
+                    name="password"
+                    id="password"
+                  />
                   <p v-if="passwordError" class="mt-2 text-sm text-red-600">
                     <span class="font-medium"></span> {{ passwordError }}
                   </p>
@@ -131,32 +158,62 @@ const handleLogin = async () => {
 
               <div class="flex items-center justify-between mt-4">
                 <div class="flex items-center me-4">
-                  <input checked id="yellow-checkbox" type="checkbox" v-model="rememberMe"
-                    class="w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 rounded focus:ring-yellow-500 focus:ring-2" />
-                  <label for="yellow-checkbox" class="ml-2 text-sm font-medium text-gray-700">Recordar</label>
+                  <input
+                    checked
+                    id="yellow-checkbox"
+                    type="checkbox"
+                    v-model="rememberMe"
+                    class="w-4 h-4 text-yellow-400 bg-gray-100 border-gray-300 rounded focus:ring-yellow-500 focus:ring-2"
+                  />
+                  <label
+                    for="yellow-checkbox"
+                    class="ml-2 text-sm font-medium text-gray-700"
+                    >Recordar</label
+                  >
                 </div>
               </div>
 
               <!-- bottom -->
               <div>
-                <button type="submit"
-                  class="text-white w-full justify-center bg-blue-700 hover:bg-blue-800 transition duration-400 ease-in-out transform font-medium rounded-lg text-lg px-4 py-3 text-center inline-flex items-center">
+                <button
+                  type="submit"
+                  class="text-white w-full justify-center bg-blue-700 hover:bg-blue-800 transition duration-400 ease-in-out transform font-medium rounded-lg text-lg px-4 py-3 text-center inline-flex items-center"
+                >
                   Iniciar sesión
                   <svg
                     class="rtl:rotate-180 w-3.5 h-3.5 ms-2 transition-transform duration-400 ease-in-out hover:translate-x-2"
-                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
                   </svg>
                 </button>
               </div>
 
-              <div v-if="authStore.error" id="alert-2"
-                class="flex items-center p-4 mb-4 border border-red-500 rounded-lg bg-white text-red-500" role="alert">
-                <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor" viewBox="0 0 20 20">
+              <div
+                v-if="authStore.error"
+                id="alert-2"
+                class="flex items-center p-4 mb-4 border border-red-500 rounded-lg bg-white text-red-500"
+                role="alert"
+              >
+                <svg
+                  class="flex-shrink-0 w-4 h-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
                   <path
-                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
+                  />
                 </svg>
                 <span class="sr-only">Info</span>
                 <div class="ms-3 text-sm font-medium">
@@ -167,7 +224,11 @@ const handleLogin = async () => {
           </div>
           <!-- footer login -->
           <div class="px-8 py-4 bg-gray-200 text-center">
-            <RouterLink class="font-medium text-blue-500 hover:text-blue-400" to="/">© Tiendas Daka™</RouterLink>
+            <RouterLink
+              class="font-medium text-blue-500 hover:text-blue-400"
+              to="/"
+              >© Tiendas Daka™</RouterLink
+            >
           </div>
         </div>
       </div>
