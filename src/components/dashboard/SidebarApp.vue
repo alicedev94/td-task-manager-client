6fbc0b708886956a4db0c7ab1c88208a562b5492
alicedev94 <alicedev94@gmail.com>
2025-01-14 10:13:33 -0400

    Adjust template

--- src/components/dashboard/SidebarApp.vue
+++ src/components/dashboard/SidebarApp.vue
@@ -5,39 +5,10 @@
     class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
     aria-label="Sidebar">
     <div class="flex flex-col justify-between h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-
-      <!-- Items -->
-   
       <PanelCo />
-
-      <div class="card flex justify-center">
-        <PanelMenu :model="items" class="w-full md:w-80">
-          <template #item="{ item }">
-            <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-              <a v-ripple class="flex items-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2"
-                :href="href" @click="navigate">
-                <span :class="item.icon" />
-                <span class="ml-2">{{ item.label }}</span>
-              </a>
-            </router-link>
-            <a v-else v-ripple class="flex items-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2"
-              :href="item.url" :target="item.target">
-              <span :class="item.icon" />
-              <span class="ml-2">{{ item.label }}</span>
-              <span v-if="item.items" class="pi pi-angle-down text-primary ml-auto" />
-            </a>
-          </template>
-        </PanelMenu>
-      </div>
-      <!-- this is singup, please add to commponent -->
       <div @click="handleLogout"
-        class="flex items-center p-2 cursor-pointer text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-        <svg
-          class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-          aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-        </svg>
+        class="flex items-center justify-center p-2 cursor-pointer text-gray-400 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <img :src="close" alt="close" :width="15" :height="15">
         <span class="flex-1 ms-3 whitespace-nowrap">Cerrar sesión</span>
       </div>
     </div>
@@ -51,6 +22,7 @@ import { useAuthStore } from '@/stores/Auth';
 import axios from 'axios';
 import router from '@/router';
 import PanelCo from '../PanelCo.vue';
+import close from '../../assets/svg/close.svg';
 
 // pinia
 const authStore = useAuthStore();
